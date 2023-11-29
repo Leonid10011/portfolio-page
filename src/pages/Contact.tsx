8dea8d748a00133bdeb0408593da8da25753ed77
Leonid10011 <leonid.budkov@gmail.com>
2023-11-29 07:23:42 +0100

    Added Contact Form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,40 @@
-export default function Contact(){
-    return(
-        <h1>Contact</h1>
-    )
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+        <p className="border rounded-md border-gray-200 shadow-lg py-4 px-1 w-full max-w-md" >Email:<br/> leonid.budkov@lbcoding.com</p>
+      <form className="mt-8 w-full max-w-md p-4 border border-gray-200 rounded-md shadow-lg">
+        <p className="font-bold text-gray-700 pb-4">Contact: </p>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+            Name
+          </label>
+          <input type="text" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+            Email
+          </label>
+          <input type="email" id="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+            Message
+          </label>
+          <textarea id="message" rows={3} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
+
+export default Contact;
