6c994ce4f17cd89da2b18000fc6549bf34aa0b85
Leonid10011 <leonid.budkov@gmail.com>
2023-11-29 07:12:57 +0100

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import { Outlet } from 'react-router'
 
 export default function App() {
 
   return (
-    <div className='w-full'>
+    <div className='flex flex-col w-full min-h-screen'>
       <Navbar/>
-      <Outlet/>
+        <div className=' self-center sm:max-w-screen-sm flex-1'>
+          <Outlet/>
+        </div>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return(
+        <div className="flex justify-around py-2 border-t-2 shadow-md border-gray-600 bottom-0">
+            <div className="text-sm">
+                2023@Leonid Budkov
+            </div>
+            <div>
+                Github: <a className="decoration-slate-200 hover:text-blue-600 hover:underline-offset-auto" href="https://www.github.com/Leonid10011">github.com/Leonid10011</a>
+            </div>
+        </div>
+    );
+}
